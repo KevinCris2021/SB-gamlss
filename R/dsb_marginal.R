795349607662811eccdb6b6bda92137f337f794a
KevinCris2021 <90438187+KevinCris2021@users.noreply.github.com>
2026-01-14 18:00:16 -0500

    Update dsb_marginal.R

--- R/dsb_marginal.R
+++ R/dsb_marginal.R
@@ -1,7 +1,13 @@
 ## --- Densidad marginal SB por integración (wrapper a C++) ---
+
+# wrapper seguro: integrate() puede pasar w vector
+cpp_joint_sb_wrap <- function(w, y, n, mu, phi) {
+  vapply(w, function(ww) cpp_joint_sb_(ww, y = y, n = n, mu = mu, phi = phi), 0.0)
+}
+
 dsb_one <- function(y, n, mu, phi) {
   integrate(
-    cpp_joint_sb_,
+    cpp_joint_sb_wrap,
     lower = 0, upper = 1,
     y = y, n = n, mu = mu, phi = phi,
     subdivisions = 2000L,
@@ -12,3 +18,4 @@ dsb_one <- function(y, n, mu, phi) {
 
 ## Vectorizada para uso interno
 dsb_vec <- Vectorize(dsb_one)
+
